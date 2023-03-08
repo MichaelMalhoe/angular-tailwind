47d6810ca316351df8a1924c2362b8c9424eb71c
Michael.Malhoe <michael.malhoe@rabobank.nl>
2023-03-08 15:12:44 +0100

    Some components that can be moved around

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MenuBarComponent } from './menu-bar/menu-bar.component';
+import { MainbodyComponent } from './mainbody/mainbody.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MenuBarComponent,
+    MainbodyComponent
   ],
   imports: [
     BrowserModule,
